75d405a14fd9f677c91c09799937249fad37f002
迟谋 <705359976@qq.com>
2019-08-26 11:11:15 +0800

    初始化

---
+++ Assets/ProjectFolderIntroduce.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectFolderIntroduce {
+    //1.UILibrary:UI库
+    //2.Vectrosity:画线工具
+    //3.TextMeshPro
+    //4.Glow11:辉光效果
+    //5.GLA_Tool:基础工具
+    //6.DigitalRain:代码雨
+}
